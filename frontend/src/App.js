3692dfb3c255aa862b9c3e02d1822526bbd39cec
Ahmedsayed322 <mamdatap@gmail.com>
2024-05-10 18:27:08 +0300

    75%

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
+import data from './data.js';
 
 function App() {
   return (
     <div>
-      <header >
+      <header>
         <a href="/">TechStore</a>
       </header>
       <main>
-        List Product
+        <h1>Featured Products</h1>
+        {data.products.map((product) => (
+          <div key={product.productid}>
+            <img src="./images/mouse.png" alt={product.name} />
+            <img src={product.img} alt={product.name} />
+          </div>
+        ))}
       </main>
     </div>
   );
 }
-
 export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: 'Razer Naga Trinity Gaming Mouse Razer Inc',
+      productid: 'Razer Naga Trinity Gaming Mouse Razer Inc',
+      category: 'accessories',
+      image: './images/mouse1.png',
+      price: 30,
+      instock: 10,
+      brand: 'Razer',
+      rating: 4.5,
+      numReview: 10,
+      description:
+        'developed by Razer Inc., is a versatile and high-performance gaming mouse tailored to meet the demands of gamers across various genres.',
+    },
+
+    {
+      name: 'Razer BlackWidow Chroma V2 Gaming keypad',
+      productid: 'Razer BlackWidow Chroma V2 Gaming keypad',
+      category: 'accessories',
+      image: './images/Keyboard1.png',
+      price: 50,
+      instock: 15,
+      brand: 'Razer',
+      rating: 4.3,
+      numReview: 20,
+      description:
+        'The Razer BlackWidow Chroma V2 Gaming Keypad offers top-tier performance and customization for gamers.',
+    },
+    {
+      name: ' Rato Spirit Of Gamer ELITE M20 Preto RGB',
+      productid: 'Rato Spirit Of Gamer ELITE M20 Preto RGB',
+      category: 'accessories',
+      image: './images/mouse2.png',
+      price: 35,
+      instock: 6,
+      brand: 'Spirit Of Gamer',
+      rating: 2,
+      numReview: 8,
+      description:
+        'The Rato Spirit Of Gamer ELITE M20 Preto RGBL is a gaming mouse with customizable RGB lighting.',
+    },
+    {
+      name: 'ASUS RGB Mechanical Gaming Keyboard',
+      productid: 'ASUS RGB Mechanical Gaming Keyboard',
+      category: 'accessories',
+      image: './images/keyboard2.png',
+      price: 40,
+      instock: 10,
+      brand: 'ASUS',
+      rating: 4.8,
+      numReview: 30,
+      description:
+        ' compact ten keyless form-factor expands to a full macro/10-key keyboard with the optional Claymore Bond',
+    },
+  ],
+};
+export default data;
